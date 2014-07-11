c37b391d89006f7ef37af74bf107daff2feb8a4b
TJ Usiyan <griotspeak@gmail.com>
2014-07-10 19:55:28 -0700

    Initial Commit

---
+++ SetOf/SetOf.h
@@ -0,0 +1,19 @@
+//
+//  SetOf.h
+//  SetOf
+//
+//  Created by TJ Usiyan on 7/10/14.
+//  Copyright (c) 2014 ButtonsAndLights. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SetOf.
+FOUNDATION_EXPORT double SetOfVersionNumber;
+
+//! Project version string for SetOf.
+FOUNDATION_EXPORT const unsigned char SetOfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SetOf/PublicHeader.h>
+
+
